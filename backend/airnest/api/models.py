021950bd3bc44039156b111a5e6c6ea27bc5df63
rajahwu <vmradford@gmail.com>
2023-11-20 08:18:01 -0500

    add basic models and serializers

--- backend/airnest/api/models.py
+++ backend/airnest/api/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=255)
+    username = models.CharField(max_length=100, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Location(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    lat = models.IntegerField()
+    lng = models.IntegerField()
+
+class Spot(models.Model):
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
+    review = models.TextField()
+    stars = models.IntegerField()
+
+class Booking(models.Model):
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+class Image(models.Model):
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, null=True, blank=True)
+    url = models.CharField(max_length=255)
--- backend/airnest/api/serializers.py
+++ backend/airnest/api/serializers.py
@@ -1,7 +1,38 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Proflie, Location, Spot, Review, Booking, Image
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'groups']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = '__all__'
+
+class SpotSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Spot
+        fields = '__all__'
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+        
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = '__all__'
