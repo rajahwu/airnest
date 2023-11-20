05b714d84fec08a77e18243e287503a3b69e8fa1
rajahwu <vmradford@gmail.com>
2023-11-20 08:22:18 -0500

    add basic views

--- backend/airnest/api/views.py
+++ backend/airnest/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
-from api.serializers import UserSerializer
+from .model import Proflie, Location, Sopt, Review, Booking, Image
+from api.serializers import UserSerializer, ProfileSerializer, LocationSerializer, SpotSerializer, ReviewSerializer, BookingSerializer, ImageSerializer
 
 
 
@@ -11,3 +12,27 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+class ProfleViewSet(viewsets.ModelViewSet):
+    queryset = Proflie.objects.all()
+    serializer_class = ProflieSerializer
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class SpotViewSet(viewsets.ModelViewSet):
+    queryset = Spot.objects.all()
+    serializer_class = SpotSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
